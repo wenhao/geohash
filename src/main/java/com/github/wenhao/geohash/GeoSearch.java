2ca442361ba79817fa7c7c30942364bf5f71c852
chenxing <woshimrf@126.com>
2015-11-28 10:36:53 +0800

    update GeoSearch to 1.7
    clone下来后发现1.8，自己是1.7，然后改了改，改完之后发现测试结果和作者的不太一样，在GeoSearchTest中的。

--- src/main/java/com/github/wenhao/geohash/GeoSearch.java
+++ src/main/java/com/github/wenhao/geohash/GeoSearch.java
@@ -30,14 +30,24 @@ public class GeoSearch {
 
     public static List<GeoRange> range(double latitude, double longitude, double range) {
         int desiredLength = getDesiredLength(range);
-        return getNineAroundCoordinate(latitude, longitude, desiredLength).stream()
-                .map(geoHash -> {
-                    long longValue = geoHash.toLong();
-                    long min = longValue << (MAX_PRECISION - desiredLength);
-                    long max = (longValue + 1) << (MAX_PRECISION - desiredLength);
-                    return new GeoRange(min, max);
-                })
-                .collect(toList());
+        // return getNineAroundCoordinate(latitude, longitude, desiredLength).stream()
+        //         .map(geoHash -> {
+        //             long longValue = geoHash.toLong();
+        //             long min = longValue << (MAX_PRECISION - desiredLength);
+        //             long max = (longValue + 1) << (MAX_PRECISION - desiredLength);
+        //             return new GeoRange(min, max);
+        //         })
+        //         .collect(toList());
+        List<GeoRange> list = new ArrayList<>();
+        List<GeoHash> nineAroundCoordinate = getNineAroundCoordinate(latitude, longitude, desiredLength);
+        for (GeoHash geoHash : nineAroundCoordinate) {
+        	long longValue = geoHash.toLong();
+            long min = longValue << (MAX_PRECISION - desiredLength);
+            long max = (longValue + 1) << (MAX_PRECISION - desiredLength);
+            list.add(new GeoRange(min, max));
+		}
+        
+        return list;
     }
 
     private static List<GeoHash> getNineAroundCoordinate(double latitude, double longitude, int desiredLength) {
@@ -48,14 +58,25 @@ public class GeoSearch {
 
     private static int getDesiredLength(double range) {
         int desiredLength = 0;
-        Optional<BigDecimal> rangeKey = PRECISION_MAP.keySet()
-                .stream()
-                .sorted()
-                .filter(bigDecimal -> bigDecimal.compareTo(BigDecimal.valueOf(range)) == 1)
-                .findFirst();
-        if (rangeKey.isPresent()) {
-            desiredLength = PRECISION_MAP.get(rangeKey.get());
-        }
+        // Optional<BigDecimal> rangeKey = PRECISION_MAP.keySet()
+        //         .stream()
+        //         .sorted()
+        //         .filter(bigDecimal -> bigDecimal.compareTo(BigDecimal.valueOf(range)) == 1)
+        //         .findFirst();
+        List<Entry<BigDecimal, Integer>> list = new ArrayList<>(PRECISION_MAP.entrySet());
+        Collections.sort(list,new Comparator<Entry<BigDecimal,Integer>>(){
+			public int compare(Entry<BigDecimal, Integer> o1, Entry<BigDecimal, Integer> o2) {
+				return o1.getKey().compareTo(o2.getKey());
+			}});
+        for (Entry<BigDecimal, Integer> entry : list) {
+        	if(entry.getKey().doubleValue()>range){
+	   			break;
+	   		}
+	   		desiredLength=entry.getValue();
+		}
+        // if (rangeKey.isPresent()) {
+        //     desiredLength = PRECISION_MAP.get(rangeKey.get());
+        // }
         return desiredLength;
     }
 }
