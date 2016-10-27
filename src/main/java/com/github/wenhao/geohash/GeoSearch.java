1a465f7c70924a3b9d2e23fe895b03cb44719f1a
Hao Wen <wenhao@126.com>
2016-10-27 08:45:36 +0800

    [Wen Hao] - base on java 1.8.

--- src/main/java/com/github/wenhao/geohash/GeoSearch.java
+++ src/main/java/com/github/wenhao/geohash/GeoSearch.java
@@ -1,43 +1,43 @@
 package com.github.wenhao.geohash;
 
-import com.github.wenhao.geohash.domain.GeoRange;
+import static java.math.BigDecimal.ROUND_HALF_UP;
+import static java.util.stream.Collectors.toList;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static com.github.wenhao.geohash.GeoHash.MAX_PRECISION;
-import static java.math.BigDecimal.ROUND_HALF_UP;
+
+import com.github.wenhao.geohash.domain.GeoRange;
 
 public class GeoSearch {
     private static final BigDecimal EARTH_RADIUS = new BigDecimal(6372797.560856);
     private static Map<BigDecimal, Integer> PRECISION_MAP;
 
     static {
-        PRECISION_MAP = new HashMap<BigDecimal, Integer>();
+        PRECISION_MAP = new HashMap<>();
         for (int angle = 1; angle <= MAX_PRECISION / 2; angle++) {
             BigDecimal bigDecimal = new BigDecimal(2)
-                    .multiply(new BigDecimal(Math.PI))
-                    .multiply(EARTH_RADIUS)
-                    .divide(new BigDecimal(2).pow(angle), ROUND_HALF_UP);
+                .multiply(new BigDecimal(Math.PI))
+                .multiply(EARTH_RADIUS)
+                .divide(new BigDecimal(2).pow(angle), ROUND_HALF_UP);
             PRECISION_MAP.put(bigDecimal, 2 * angle);
         }
     }
 
     public static List<GeoRange> range(double latitude, double longitude, double range) {
         int desiredLength = getDesiredLength(range);
-        List<GeoHash> nineAroundCoordinates = getNineAroundCoordinate(latitude, longitude, desiredLength);
-        List<GeoRange> geoRanges = new ArrayList<GeoRange>();
-        for (GeoHash geoHash : nineAroundCoordinates) {
-            long longValue = geoHash.toLong();
-            long min = longValue << (MAX_PRECISION - desiredLength);
-            long max = (longValue + 1) << (MAX_PRECISION - desiredLength);
-            geoRanges.add(new GeoRange(min, max));
-        }
-        return geoRanges;
+        return getNineAroundCoordinate(latitude, longitude, desiredLength).stream()
+            .map(geoHash -> {
+                long longValue = geoHash.toLong();
+                long min = longValue << (MAX_PRECISION - desiredLength);
+                long max = (longValue + 1) << (MAX_PRECISION - desiredLength);
+                return new GeoRange(min, max);
+            })
+            .collect(toList());
     }
 
     private static List<GeoHash> getNineAroundCoordinate(double latitude, double longitude, int desiredLength) {
@@ -48,17 +48,13 @@ public class GeoSearch {
 
     private static int getDesiredLength(double range) {
         int desiredLength = 0;
-        BigDecimal rangeKey = null;
-        List<BigDecimal> ranges = new ArrayList<BigDecimal>(PRECISION_MAP.keySet());
-        Collections.sort(ranges);
-        for (BigDecimal bigDecimal : ranges) {
-            if (bigDecimal.compareTo(BigDecimal.valueOf(range)) == 1) {
-                rangeKey = bigDecimal;
-                break;
-            }
-        }
-        if (rangeKey != null) {
-            desiredLength = PRECISION_MAP.get(rangeKey);
+        Optional<BigDecimal> rangeKey = PRECISION_MAP.keySet()
+            .stream()
+            .sorted()
+            .filter(bigDecimal -> bigDecimal.compareTo(BigDecimal.valueOf(range)) == 1)
+            .findFirst();
+        if (rangeKey.isPresent()) {
+            desiredLength = PRECISION_MAP.get(rangeKey.get());
         }
         return desiredLength;
     }
